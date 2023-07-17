21d78f6c21bd262ce843cf3bfd8408b263ee430f
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-07-17 14:42:50 +0200

    pass port and node's mode as arg to the process

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use node::new_node;
 use ed25519_dalek::{Keypair, PublicKey, SecretKey};
 use rand::rngs::OsRng;
+use core::panic;
+use std::env;
 
 mod node;
 mod message;
 mod connection;
 
+use message::Mode::{LEADER, FOLLOWER};
+
 fn main() {
-    run_node()
+    // first arg - node's mode (leader/follower), second - port
+    let args: Vec<String> = env::args().collect();
+
+    let mode = {
+        let mode_arg = args[1].as_str();
+        match mode_arg {
+            "leader" => LEADER,
+            "follower" => FOLLOWER,
+            _ => panic!("Invalid mode {mode_arg}, should be either `follower` or `leader`")
+        }
+    };
+    
+    let port = String::from(&args[2]);
+
+    run_node(mode, port);
 }
 
-fn run_node() {
-    let mode = message::Mode::FOLLOWER;
-    let port = String::from("8000");
+fn run_node(mode: message::Mode, port: String) {
     let (pubkey, privkey) = gen_keypair();
 
     let node = new_node(pubkey, privkey, mode, port);
