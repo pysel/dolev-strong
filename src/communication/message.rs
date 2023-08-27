8c67da2ceebc4a869410332ce3ece1c98c3598a3
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-08-27 13:22:10 +0200

    consensus msg + msg interface

--- src/communication/message.rs
+++ src/communication/message.rs
@@ -1,8 +1,12 @@
 #![allow(dead_code)]
-use ed25519_dalek::PublicKey;
+use ed25519_dalek::{PublicKey, Signature, Keypair};
 
 pub mod serde;
 pub mod types;
+
+pub trait MessageI {
+    fn serialize(&self, keypair: &Keypair, config_index: i32);
+}
 
 // A binary value all honest nodes must agree on
 pub enum Value {
@@ -10,7 +14,12 @@ pub enum Value {
     One,
 }
 
-pub struct ConsensusMsg(Value);
+pub struct ConsensusMsg { 
+    proposed_value: Value,
+    supporting_signatures: Vec<Signature>,
+}
+
+pub struct ProposeMsg(Value);
 
 pub struct PubkeyBroadcastMsg(PublicKey);
 pub fn new_pk_broadcast_msg(pk: PublicKey) -> PubkeyBroadcastMsg {
