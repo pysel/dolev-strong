301c29940606a7cb222d2cc9600d129944968c8f
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-09-30 12:13:48 -0700

    abstract proposal message validation in follower's genesis strategy

--- src/consensus/genesis/strategy.rs
+++ src/consensus/genesis/strategy.rs
@@ -4,6 +4,6 @@ pub mod follower;
 pub mod leader;
 
 pub trait GenesisStrategy {
-    // genesis_stage is a "zero"'th stage, when a leader proposes a value
+    // genesis_stage is the logic for the genesis stage of the consensus protocol taken by a node depending on it's mode.
     fn genesis_stage(&self, self_node: ConsensusNode);
 }
--- src/consensus/genesis/strategy/follower.rs
+++ src/consensus/genesis/strategy/follower.rs
@@ -1,5 +1,5 @@
 use super::GenesisStrategy;
-use crate::consensus::ConsensusNode;
+use crate::{consensus::{ConsensusNode, protocol::convincing::ConsensusMsgReceivedTuple}, communication::message::ConsensusMsg};
 
 pub struct FollowerStrategy;
 
@@ -11,22 +11,32 @@ impl GenesisStrategy for FollowerStrategy {
         
         let received_messages = self_node.receive_all_consensus_messages();
         println!("Received messages during genesis: {:?}", received_messages);
-        if received_messages.len() != 1 {
-            // TODO: this is byzantine behavior, should not panic
-            panic!("more than one messages received at proposal stage");
-        }
-        let received_message = received_messages[0].clone();
-
-        if !received_message.convincing(&self_node) {
-            panic!("received proposal is not convincing") // TODO: Output default value - sender is Byzantine
-        }
 
         // since the message is convincing, we can directly unwrap the received message
-        let consensus_msg = received_message.1.unwrap().to_consensus_msg();
+        let consensus_msg = validate_messages_received_on_proposal(received_messages, &self_node);
 
         println!("Received convincing proposal, broadcasting: {:?}", consensus_msg);
         self_node.communication.broadcast_message(&consensus_msg);
 
         self_node.enter_stage(1);
     }
+}
+
+// validate_messages_received_on_proposal checks that the messages received on proposal stage are valid and returns the consensus message
+// to be broadcasted to peers if the proposal is convincing
+fn validate_messages_received_on_proposal(msgs: Vec<ConsensusMsgReceivedTuple>, cnode: &ConsensusNode) -> ConsensusMsg {
+    // should only receive one message
+    if msgs.len() != 1 {
+        // TODO: this is a byzantine behavior, need to find a message that came from a leader, and discard others
+        panic!("more than one messages received at proposal stage");
+    }
+
+    let received_message: ConsensusMsgReceivedTuple<'_> = msgs[0].clone();
+    if !received_message.convincing(&cnode) {
+        panic!("received proposal is not convincing") // TODO: Output default value - sender is Byzantine
+    }
+
+    // since the message is convincing, we can directly unwrap the received message
+    let consensus_msg: crate::communication::message::ConsensusMsg = received_message.1.unwrap().to_consensus_msg();
+    consensus_msg
 }
